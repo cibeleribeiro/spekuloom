3df5140bdcaa4d3f7069c08510603e61148bbeca
Carlo E. T. Oliveira <carlo@nce.ufrj.br>
2018-05-21 01:06:57 -0300

    survey and plot tagged clauses for multiple windows

--- src/spekuloom/main.py
+++ src/spekuloom/main.py
@@ -2,7 +2,7 @@ import operator
 
 import nltk
 from nltk import *
-from nltk.book import text2
+from nltk.book import *
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,34 +11,76 @@ from util import Mont
 
 class Clauses:
     def __init__(self):
-        self.text = text = ' '.join(text2[400:1500])
         self.token = self.punct = self.clauses = ''
         self.punctuation = list(",.:;!?")+["CC", "--"]
-        punctuation = list(",.:;!?-")
-        # print(self.punctuation)
-        for pt in punctuation:
+        self.punctuate = list(",.:;!?-")
+        _text = self.text_setup(text5)
+        self.split_clauses(_text)
+        # self.plot_clause('a')
+        self.classes = Mont().mont_symbol()
+        # self.marker()
+        self.survey_corpora()
+        # self.survey_patterns()
+
+    def text_setup(self, text):
+        text = ' '.join(text[400:2500])
+        for pt in self.punctuate:
             text = text.replace(" {} ".format(pt), "{} ".format(pt))
         text = text.replace("--", u"\u2014")
         text = text.replace("- ", "-")
         text = text.replace(" ' ", "'")
         text = text.replace("Mr.", "Mrp")
-        self.text = text
-        self.split_clauses()
-        # self.plot_clause('a')
-        self.classes = Mont().mont_symbol()
-        # self.marker()
-        self.survey_patterns()
+        return text
 
-    def survey_patterns(self):
+    def survey_corpora(self, wind=3):
+        corpora = []
+        dcorpora = {}
+        _texts =[text1, text2, text3, text4]
+        for ind, txt in enumerate(_texts):
+            _text = self.text_setup(txt)
+            self.split_clauses(_text)
+            dcorpora[ind] = {k: v for k, v in self.survey_patterns(wind)}
+
+            corpora.extend(self.survey_patterns(wind))
+
+        self.patterns = {}
+        for pat in corpora:
+            self.patterns[pat] = self.patterns.setdefault(pat, 0)+1
+        sorted_pattern = [(shape, count)
+                          for shape, count in sorted(
+                self.patterns.items(), key=operator.itemgetter(1), reverse=True)][:60]
+        for shape, count in sorted_pattern:
+            print(shape[0], end='')
+            print(count)
+        for shape, count in dcorpora[3].items():
+            print(shape[0], end='')
+            print(count)
+        marks = 'rs gs bs ms r^ g^ b^ m^ c^'.split()
+        _xpat = list(k for k, _ in sorted_pattern)
+        patt = [(_xpat, [dcorpora[ind].setdefault(_x, 0) for _x, _ in _xpat],
+                 marks[ind]) for ind in range(len(_texts))]
+        labels = ["".join(list(l)[x] for x in range(7, wind*14, 14)) for l, _ in _xpat]
+        print(labels)
+        self.plot_patterns(patt, labels)
+
+    def survey_patterns(self, wind=5):
+        def to_shapes(shapes):
+            return "".join(self.classes.get(shape[:2], self.classes['ZZ']) for shape in shapes.split(":"))
+
         def window(clause):
-            return zip(clause, clause[1:], clause[2:])
-        patterns = [(a, b, c) for a_clause in self.clauses for (_, a), (_, b), (_, c) in window(a_clause)]
+            return [":".join(x) for x in zip(*[[_tag for _, _tag in clause[ind:]]for ind in range(wind)])]
+            # return zip(clause, clause[1:], clause[2:])
+        patterns = [pat for a_clause in self.clauses for pat in window(a_clause)]
         self.patterns = {}
         for pat in patterns:
             self.patterns[pat] = self.patterns.setdefault(pat, 0)+1
-        print(sorted(self.patterns.items(), key=operator.itemgetter(1), reverse=True))
+        sorted_pattern = [(to_shapes(shape), count)
+                          for shape, count in sorted(self.patterns.items(), key=operator.itemgetter(1), reverse=True)]
+        # for shape, count in sorted_pattern[:60]:
+        #     print(shape, end='')
+        #     print(count)
         # print(self.patterns)
-
+        return sorted_pattern
 
     def marker(self):
         print(self.text)
@@ -50,8 +92,8 @@ class Clauses:
         # mak_claus = [tag + wd for wd, tag in clause]
         print(' '.join(mak_claus))
 
-    def split_clauses(self):
-        token = word_tokenize(self.text)
+    def split_clauses(self, text):
+        token = word_tokenize(text)
         self.token = token = nltk.pos_tag(token)
         punctuation=",.:;!?CCIN--"
         # self.punct = [(index, punct) for index, (txt, punct) in enumerate(self.token) if punct in self.punctuation]
@@ -60,7 +102,7 @@ class Clauses:
                 for punct in self.punctuation]
         self.clauses = [token[start + (0 if token[start][1] == "CC" else 1): stop]+[self.token[stop]]
                         for start, stop in list(zip(pun, pun[1:])) if stop > (start+1)]
-        # print(self.clauses)
+        return self.clauses
 
     def plot_clause(self, a_clause):
         fig = plt.figure()
@@ -87,6 +129,30 @@ class Clauses:
         # Show the boundary between the regions:
         # theta = np.arange(0, np.pi / 2, 0.01)
         # plt.plot(r0 * np.cos(theta), r0 * np.sin(theta))
+
+        plt.show()
+
+    def plot_patterns(self, patt, labels):
+        fig = plt.figure()
+        fig.suptitle('bold figure subtitle', fontsize=14, fontweight='bold')
+
+        ax = fig.add_subplot(111)
+        fig.subplots_adjust(top=0.85)
+        ax.set_title('axes title')
+        ax.set_xticklabels(labels)
+        plt.xticks(list(range(0, len(labels), 2)))
+        # handles = ax.get_legend()
+        handles, labels = ax.get_legend_handles_labels()
+        labels = ['title{}'.format(range(len(patt)))]
+        ax.legend(handles, labels)
+        # plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+        ax.set_xlabel('xlabel')
+        ax.set_ylabel('ylabel')
+
+        for _x, _y, _c in patt:
+            plt.plot(range(len(_y)), _y, _c)
+        plt.legend(["moby", "sense", "genesis", "address"], loc='upper right')
 
         plt.show()
 
