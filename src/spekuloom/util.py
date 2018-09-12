1054b55ea17f4f68ff8900b0698d23bf43979e1f
cibele <cici.rco@gmail.com>
2018-09-11 23:59:01 -0300

    tags novas

--- src/spekuloom/util.py
+++ src/spekuloom/util.py
@@ -110,6 +110,32 @@ class Mont:
         # for a, b in classes.items():print(b)
         return classes
 
+    def mont_symbol_pt(self):
+
+        # classes=dict(noun,article,adjective, verb, adverb, preposition, conjuction,interjection, pronoun)
+        classes = dict(NOU="\033[1;30m" + u"\u25B2" + "\033[1;0m",
+                       DET="\033[1;36m" + u"\u25B4" + "\033[1;0m",
+                       ADJ="\033[1;34m" + u"\u25ED" + "\033[1;0m",
+                       VER="\033[1;31m" + u"\u25CF" + "\033[1;0m",
+                       MD="\033[1;31m" + u'\u25cc' + "\033[1;0m",
+                       ADV="\033[1;31m" + u"\u25CB" + "\033[1;0m",
+                       ADP="\033[1;32m" + u"\u25E1" + "\033[1;0m",
+                       CON="\033[1;35m" + u"\u25AC" + "\033[1;0m",
+                       UH="\033[1;33m" + u"\u25C8" + "\033[1;0m",
+                       PRO="\033[1;35m" + u"\u25BB" + "\033[1;0m",
+                       ZZ="\033[1;33m" + u"\u25A4" + "\033[1;0m",
+                       WD="\033[1;36m" + u"\u25A5" + "\033[1;0m",
+                       WP="\033[1;35m" + u"\u25A6" + "\033[1;0m",
+                       WR="\033[1;31m" + u"\u25A7" + "\033[1;0m",
+                       TO="\033[1;32m" + u"\u25A8" + "\033[1;0m",
+                       RP="\033[1;35m" + u"\u25A9" + "\033[1;0m",
+                       PD="\033[1;36m" + u"\u25F0" + "\033[1;0m",
+                       PO="\033[1;30m" + u"\u25F1" + "\033[1;0m",
+                       EX="\033[1;33m" + u"\u25F2" + "\033[1;0m",
+                       NUM="\033[1;34m" + u"\u25F3" + "\033[1;0m")
+        # for a, b in classes.items():print(b)
+        return classes
+
 
 if __name__ == '__main__':
     b = Mont().carrega_livros()
